d9775b28b943d7f875fd5b620f4a37dccf83295a
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-24 22:20:49 +0530

    [Sartaj] : Added Password rule 4 program

--- Regex/PasswordRules.cs
+++ Regex/PasswordRules.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Regex01
 {
+
     public class PasswordRules
     {
         public Regex Password_Regex = new Regex("^[A-Za-z0-9]{8,16}$");
@@ -46,6 +47,21 @@ namespace Regex01
             string password3 = Console.ReadLine();
 
             if (Password_Regex3.IsMatch(password3))
+            {
+                Console.WriteLine("The password is valid ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid passord");
+            }
+        }
+        public Regex Password_Regex4 = new Regex("^[A-Za-z]{6,}[A-Z]{1,}[0-9]{1,}[-~!@#$%^*()_+{}:|?`;',]{1,}$");
+        public void ValidatePassword4()
+        {
+            Console.Write("Enter the password : ");
+            string password4 = Console.ReadLine();
+
+            if (Password_Regex4.IsMatch(password4))
             {
                 Console.WriteLine("The password is valid ");
             }
